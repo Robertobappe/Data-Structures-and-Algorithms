8a4e8740979b9abaf2bf2348ff234fe917d6f97b
Roberto Silva <robertobs@usp.br>
2025-04-04 20:32:07 -0300

    example1

---
+++ BigO/example1.js
@@ -0,0 +1,17 @@
+//O(n)
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(10000).fill('nemo');
+
+function findNemo1(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+  let t = performance.now();
+  console.log('Call to find Nemo took ' + (t-t0) + ' milliseconds');
+}
+
+findNemo1(large);
