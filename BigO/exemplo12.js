14e5327b5e3f643ecc783627f0d72e607a019784
Roberto Silva <robertobs@usp.br>
2025-04-08 23:48:24 -0300

    vf1

---
+++ BigO/exemplo12.js
@@ -0,0 +1,13 @@
+//Given 2 arrays, create a function that let's a user know (true/false)
+//whether these two arrays contain any comomom items
+//For example:
+//const array1 = ['a','b','c','x'];
+//const array2 = ['z','y','i'];
+//should return false
+//-------------------------------------------------------------------
+//const array1 = ['a','b','c','x'];
+//const array2 = ['z','y','i'];
+//should return true
+
+//2 parameters - arrays - no size limit
+//return true or false
