045eb8d4cd0341d125edcfee6c6284541aa02032
Roberto Silva <robertobs@usp.br>
2025-04-09 23:44:41 -0300

    vf2

--- BigO/exemplo12.js
+++ BigO/exemplo12.js
@@ -12,7 +12,7 @@ const array2 = ['z','y','x'];
 //2 parameters - arrays - no size limit
 //return true or false
 
-//not a better solution - brute force
+//not a better solution - brute force --> O(a*b)
 function containsCommonItem(arr1,arr2){
     for(let i=0; i<arr1.length;i++){
         for(let j=0; j<arr2.length;j++){
@@ -23,5 +23,21 @@ function containsCommonItem(arr1,arr2){
     }
     return false;}
 
-retorno = containsCommonItem(array1,array2);
-console.log(retorno);
+//retorno = containsCommonItem(array1,array2);
+//console.log(retorno);
+
+//-------------------------------------------------------------------
+// let's create a hase table or in javascript called object
+// array1 ==> obj{
+// a: true,
+// b: true,
+// c: true,
+// x: true
+//}
+// array2[index] === obj.properties
+
+function containsCommonItem2(arr1,arr2){
+    //loop through first array and create object where properties === items inte array
+    //loop through second array and check if item in second array exists on created object
+    //O(a+b)
+}
