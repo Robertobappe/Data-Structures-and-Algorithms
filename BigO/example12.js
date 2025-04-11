017d019e3676a6648ae00fc6e616738c8b124f2b
Roberto Silva <robertobs@usp.br>
2025-04-10 21:21:08 -0300

    vf

--- BigO/example12.js
+++ BigO/example12.js
@@ -12,7 +12,8 @@ const array2 = ['z','y','x'];
 //2 parameters - arrays - no size limit
 //return true or false
 
-//not a better solution - brute force --> O(a*b)
+//not a better solution - brute force --> O(a*b) Time complexity
+//O(1) - Space complexity
 function containsCommonItem(arr1,arr2){
     for(let i=0; i<arr1.length;i++){
         for(let j=0; j<arr2.length;j++){
@@ -21,7 +22,8 @@ function containsCommonItem(arr1,arr2){
             }
         }
     }
-    return false;}
+    return false;
+}
 
 //retorno = containsCommonItem(array1,array2);
 //console.log(retorno);
@@ -36,8 +38,26 @@ function containsCommonItem(arr1,arr2){
 //}
 // array2[index] === obj.properties
 
+//O(a+b) - Time complexity
+//O(a) - Space complexity
 function containsCommonItem2(arr1,arr2){
     //loop through first array and create object where properties === items inte array
+    //note: we can modularized this part of the code --> function to transfor into a map
+    let map = {};
+    for(let i=0; i<arr1.length;i++){
+        if(!map[arr1[i]]){
+            const item = arr1[i];
+            map[item] = true;
+        }
+    }
     //loop through second array and check if item in second array exists on created object
-    //O(a+b)
+    for(let j=0; j<arr2.length; j++){
+        if(map[arr2[j]]){
+            return true;
+        }
+    }
+    return false;
 }
+
+a=containsCommonItem2(array1,array2);
+console.log(a);
