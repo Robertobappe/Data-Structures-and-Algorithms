bf33aa3007a74ab01350f4646dd0c1a13521170f
Roberto Silva <robertobs@usp.br>
2025-06-14 22:01:39 -0300

    my version

---
+++ Algorithms/Sorting/insertionSort.py
@@ -0,0 +1,40 @@
+from typing import List
+class solution():
+    def insertionSort(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        if n <= 1:
+             return nums
+        
+        for i in range(n - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                if i > 0:
+                    prev = 0
+                    change = True
+                    while change:
+                            change = False
+                            if ((i - prev - 1 >= 0) and (nums[i - prev - 1] > nums[i - prev])):
+                                 nums[i - prev - 1], nums[i - prev] = nums[i - prev], nums[i - prev - 1]
+                                 prev += 1
+                                 change = True
+        return nums
+
+
+def main():
+     test_cases = [
+          [3,0,1,8,7,2,5,4,9,6],
+          [5432,5,4,86,2,1,35,0],
+          [5,12,98,-1,45,4,8,3],
+          [12, 11, 13, 5, 6],
+          [5, 1, 4, 2, 8]
+     ]
+
+     sol = solution()
+     for i in test_cases:
+          print(f'Input  {i}')
+          rel = sol.insertionSort(i)
+          print(f'Output {rel}')
+
+if __name__ == "__main__":
+     main()
